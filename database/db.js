12b18f82086443cbb774064f8782c8771dfeac86
ujang <jabbarujang@gmail.com>
2023-01-28 00:46:13 +0700

    recheck

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME || 'qoneqo'
 })
 
 pool.query = util.promisify(pool.query).bind(pool);
